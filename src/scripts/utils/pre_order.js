749336878925c12bc4576ab0f8dec03ce5160e09
ObserverMoment <bosonsoul@gmail.com>
2020-02-13 09:30:16 +0000

    Turn off pre-order after CJ38 arrival

--- src/scripts/utils/pre_order.js
+++ src/scripts/utils/pre_order.js
@@ -1,45 +1,7 @@
-const availableForPreOrder = [
-  'dice500',
-  'wpc300high',
-  'wpc500high',
-  'rgfdealer213blue',
-  'rgfdealer213black',
-  'rgfpremier213black',
-  'rgfpremier213blue',
-  'rgfpremier213green',
-  'rgfpro213green',
-  'rgfpro213black',
-  'rgftourn213green',
-  'pt2400black',
-  'pt3600black',
-  'miniptblack',
-  'rtminigreen',
-  't800black',
-  't800green',
-  'htop500blue',
-  'htop500black',
-  'htop500green',
-  'cs500black',
-  'cs500green',
-  'cs500blue',
-  'rt400black',
-  'rt300black',
-  'rt200green',
-  'ptf301black',
-  'ptf301blue',
-  'ptf301green',
-  'rgfpro213blue',
-  'rgftourn213blue',
-  'rgftourn213black',
-  'pt2400blue',
-  'rtminiblue',
-  'tourneypro500low',
-  'tourneypro1000high',
-  'propoker500high'
-]
+const availableForPreOrder = []
 
 function getDueDate () {
-  return 'February 7th'
+  return 'February 12th'
 }
 
 function isPreOrder (sku) {
